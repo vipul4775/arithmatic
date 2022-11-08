147e8e0e968152c3ea691451a6d78106467af7cf
tiwari-vipul <113029842+tiwari-vipul@users.noreply.github.com>
2022-11-08 16:03:45 +0530

    demo to publish on maven central

---
+++ src/main/java/com/github/vipultiwari45/arithmatic/TestLib.java
@@ -0,0 +1,22 @@
+package com.github.vipultiwari45.arithmatic;
+
+public class TestLib {
+    public int getSum(int a,int b)
+    {
+
+        System.out.println("Hi You are in custom library& Sum method");
+        return a+b;
+    }
+    public int getDiff(int a,int b)
+    {
+
+        System.out.println("Hi You are in custom library& substract method");
+        return a-b;
+    }
+    public int getProduct(int a,int b)
+    {
+
+        System.out.println("Hi You are in custom library& multiplication method");
+        return a*b;
+    }
+}
